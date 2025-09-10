2699b5143396dcfe95c1ad04f8b06e99a04c27dc
synth1212 <224894999+synth1212@users.noreply.github.com>
2025-09-09 20:36:46 -0400

    made edits to hcpcs_processor.py and .gitignore and .venv files

--- scripts/hcpcs/hcpcs_processor.py
+++ scripts/hcpcs/hcpcs_processor.py
@@ -1,10 +1,28 @@
 import pandas as pd
+import numpy as np
 
 # Path to the HCPCS text file
-file_path = "Module1_MedicalCodexes/hcpcs/HCPC2025_OCT_ANWEB.txt"
+file_path = "scripts/hcpcs/HCPC2025_OCT_ANWEB_v3.xlsx"
 
-# Read the file into a DataFrame
-# The file appears to be fixed-width formatted, so we'll use read_fwf
+# Print Column Names based on dataset
+print("Column Names based on dataset:")
+# Show first column of data from the dataset; Read the file into a DataFrame
+df=pd.read_excel(file_path)
+print(df.columns)
+# Output:
+    Index(['HCPC', 'SEQNUM', 'RECID', 'LONG DESCRIPTION', 'SHORT DESCRIPTION',
+       'PRICE1', 'PRICE2', 'PRICE3', 'PRICE4', 'MULT_PI', 'CIM1', 'CIM2',
+       'CIM3', 'MCM1', 'MCM2', 'MCM3', 'STATUTE', 'LABCERT1', 'LABCERT2',
+       'LABCERT3', 'LABCERT4', 'LABCERT5', 'LABCERT6', 'LABCERT7', 'LABCERT8',
+       'XREF1', 'XREF2', 'XREF3', 'XREF4', 'XREF5', 'COV', 'ASC_GRP', 'ASC_DT',
+       'OPPS', 'OPPS_PI', 'OPPS_DT', 'PROCNOTE', 'BETOS', 'TOS1', 'TOS2',
+       'TOS3', 'TOS4', 'TOS5', 'ANEST_BU', 'ADD DT', 'ACT EFF DT', 'TERM DT',
+       'ACTION CD'],
+      dtype='object')
+
+# Select the columns you want to keep 
+
+    lonic_small = lonic[list_cols]
 
 def load_hcpcs_to_df(file_path):
     # You may need to adjust colspecs based on actual column widths
@@ -12,8 +30,7 @@ def load_hcpcs_to_df(file_path):
     colspecs = [(0, 11), (11, 90), (90, 180), (180, 200), (200, 220), (220, 240), (240, 260), (260, 280)]
     column_names = [
         "Code", "Description1", "Description2", "Type", "Unknown1", "Unknown2", "Unknown3", "Unknown4"
-    ]
-    df = pd.read_fwf(file_path, colspecs=colspecs, names=column_names)
+    ]    df = pd.read_fwf(file_path, colspecs=colspecs, names=column_names)
     return df
 
 if __name__ == "__main__":
