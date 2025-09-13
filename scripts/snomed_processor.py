a94764feb319d956c236e1f6f6eea3a01e9e31fd
synth1212 <224894999+synth1212@users.noreply.github.com>
2025-09-12 21:11:54 -0400

    finalized snomed_processor.py

--- scripts/snomed_processor.py
+++ scripts/snomed_processor.py
@@ -1,8 +1,15 @@
 import polars as pl
 from pathlib import Path
+import os
+import sys
 
-file_path = Path('Module1_MedicalCodexes/snowmed/sct2_Description_Full-en_US1000124_20250301.txt')
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__name__), ".."))) 
+from utils.common_functions import save_to_format
 
+# Path to Raw Data 
+file_path = Path('inputs/sct2_Description_Full-en_US1000124_20250301.txt')
+
+# Read and load the raw data with delimiter of [/t] and assign column headers
 df = pl.read_csv(
     file_path,
     separator='\t',
@@ -23,12 +30,34 @@ df = pl.read_csv(
     }
 )
 
-output_dir = Path('Module1_MedicalCodexes/snowmed/output')
-output_dir.mkdir(exist_ok=True)
-output_path = output_dir / 'sct2_Description_Full.csv'
+# Select the columns you want to keep
+cols = ["term"]
+snomed_small = df[cols]
+print(snomed_small.head())
 
+# Clean data by replacing the multiple spacing with just a single spacing
+snomed_small["term"].str.strip_chars().str.replace_all(r"\s+", " ").alias("term")
+
+# Keep only unique code values to eliminates potential duplicate codes
+snomed_small = snomed_small.unique(subset=['term'])
+
+# Add a last_updated column
+df = df.with_columns(pl.lit("2025-09-10").alias("last_updated"))
+
+# Save as CSV to outputs
+output_path = 'outputs/sct2_Description_Full-en_US1000124_20250301.csv'
 df.write_csv(output_path)
 
+# Save as CSV to output with common function
+save_to_format(df, baseFile="sct2_Description_Full-en_US1000124_20250301")
+
+# Check the output file size 
+file_path2 = "outputs/sct2_Description_Full-en_US1000124_20250301.csv"
+size_bytes = os.path.getsize(file_path2)
+size_mb = size_bytes / (1024 * 1024)
+print(f"CSV size: {size_bytes:,} bytes ({size_mb:.2f} MB)")
+
+# Print summary statistics
 print(f"Successfully parsed {len(df)} records from SNOMED CT file")
 print(f"Saved to {output_path}")
 print(f"Dataset shape: {df.shape}")
